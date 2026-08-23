fb6e38732bc34ff41457523a1f7102505ccc9f78
DamonsJ <shlkl99@163.com>
2026-08-23 08:39:56 +0000

    Deploying to gh-pages from @ DamonsJ/DamonsJ.github.io@40afd5a15261cd2c6a30365d2fb56ba6eee892fb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -320,8 +320,8 @@ ninja.data = [{
             },},{id: "reading-读-通向蜘蛛巢的小径-之前-卡尔维诺-抵抗运动与一个孩子眼中的战争",
           title: '读《通向蜘蛛巢的小径》之前：卡尔维诺、抵抗运动与一个孩子眼中的战争',
           taxonomySource: "_reading/the-path-to-the-spiders-nests.md",
-          keywords: "小说 文学 卡尔维诺 意大利抵抗运动",
-          description: " 小说 文学 卡尔维诺 意大利抵抗运动",
+          keywords: "小说 文学 卡尔维诺",
+          description: " 小说 文学 卡尔维诺",
           section: "Reading",handler: () => {
               window.location.href = "/reading/the-path-to-the-spiders-nests.html";
             },},{
